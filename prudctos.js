e9dc471ffcc5847936c5dbd4eb402c90c6d87ca5
juanvolpee <138537881+juanvolpee@users.noreply.github.com>
2023-08-06 16:12:58 -0300

    Create prudctos.js

---
+++ prudctos.js
@@ -0,0 +1,55 @@
+const productos = [{
+    id: 1,
+    foto: "https://www.casa-segal.com/wp-content/uploads/2020/03/coca-cola-225L-almacen-gaseosas-casa-segal-mendoza-600x600.jpg",
+    nombre: "Coca Cola 2.25 lt",
+    precio: 1000
+},
+{
+    id: 2,
+    foto: "https://carrefourar.vtexassets.com/arquivos/ids/348649/7790895001000_E02.jpg?v=638241667388330000",
+    nombre: "Sprite 2.25 lt",
+    precio: 1000
+},
+{
+    id: 3,
+    foto: "https://www.menosuno.com.ar/database/articulos/fotos/207/ENERGIZANTE%20SPEED%20CHICO%20X%20269%20CC__1.jpg",
+    nombre: "Speed Chico",
+    precio: 500
+},
+{
+    id: 4,
+    foto: "https://www.tiendafacil.com.ar/tienda/uploads/600x600/1659452098_a6d31adb.jpg",
+    nombre: "Speed Grande",
+    precio: 700
+},
+{
+    id: 5,
+    foto: "https://soloporgusto.com/wp-content/uploads/2022/02/destaca-SKYY_Summer_Edition_2.jpg",
+    nombre: "Vodka SKY",
+    precio: 2800
+},
+{
+    id: 6,
+    foto: "https://alberdisa.vteximg.com.br/arquivos/ids/169964-1000-1000/fernet-branca-750.jpg?v=637855404316330000",
+    nombre: "Fernet Branca 750 ml",
+    precio: 3000
+},
+{
+    id: 7,
+    foto: "https://www.brancastore.com.ar/142-large_default/fernet-branca-450ml.jpg",
+    nombre: "Fernet Branca 450 ml",
+    precio: 1800
+},
+{
+    id: 8,
+    foto: "https://cdn11.bigcommerce.com/s-3stx4pub31/images/stencil/1280x1280/products/2594/6413/gancia__48145.1655742450.jpg?c=2",
+    nombre: "Gancia",
+    precio: 1500
+},
+{
+    id: 9,
+    foto: "https://hielosiglu.com/wp-content/uploads/2019/03/bolsa-de-hielo.png",
+    nombre: "Hielo 3kg",
+    precio: 500
+}
+];
